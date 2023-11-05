69d3d518dc54cf699982a5c1e81284721cc8fed3
AndreSantos03 <andrebernardosantos@gmail.com>
2023-11-05 23:03:29 +0000

    dada

--- src/main.pl
+++ src/main.pl
@@ -14,27 +14,30 @@ game(FinalBoard) :-
     start_menu(TypePlayer1,TypePlayer2),
     play_game(InitialBoard, 1, FinalBoard,TypePlayer1,TypePlayer2).
 
-
-play_game(Board, Color, FinalBoard,TypePlayer1,TypePlayer2) :-
-    print_board(Board),
-    get_color(Color,ColorName),
-    format('It''s the ~w''s pieces turn to move!',ColorName),nl,
-    format('Color=~d,typeplayer2:~d',[Color,TypePlayer2]),nl,
-
-    move_handler(Board,Color,TempBoard,TypePlayer1,TypePlayer2,I1-J1),
-    switch_color(Color, NextColor),
-    (is_game_over(I1-J1) -> FinalBoard = TempBoard ; play_game(TempBoard, NextColor, FinalBoard,TypePlayer1,TypePlayer2)).
+    play_game(Board, Color,FinalBoard,TypePlayer1,TypePlayer2) :-
+        print_board(Board),
+        get_color(Color,ColorName),
+        format('It''s the ~w''s pieces turn to move!',ColorName),nl,
+        format('Color=~d,typeplayer2:~d',[Color,TypePlayer2]),nl,
+        (has_available_moves(Board, Color, AvailableMoves) ->
+            move_handler(Board,Color,TempBoard,TypePlayer1,TypePlayer2,I1-J1),
+            switch_color(Color, NextColor),
+            (reach_goal(TempBoard, Color) -> game_over(TempBoard, Winner) ; play_game(TempBoard, NextColor,FinalBoard,TypePlayer1,TypePlayer2))
+        ;
+            game_over(Board, Winner)
+        ).
+    
 
 
 move_handler(Board, 1, FinalBoard, 1, TypePlayer2, I1-J1) :-
     get_available_moves(Board, Color, AvailableMoves),
     print_available_moves(AvailableMoves),
-    move(Board, FinalBoard, I1-J1).
+    move(Board,1, FinalBoard).
 
 move_handler(Board, 2, FinalBoard, TypePlayer1, 1, I1-J1) :-
     get_available_moves(Board, Color, AvailableMoves),
     print_available_moves(AvailableMoves),
-    move(Board, FinalBoard, I1-J1).
+    move(Board,2, FinalBoard).
 
 
 move_handler(Board, 1, FinalBoard, 2, TypePlayer2, I1-J1) :-
